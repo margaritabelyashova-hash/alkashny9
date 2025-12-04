7bf9b23c51baff58ccf4a98a8e3ce8ade7384536
margaritabelyashova-hash <margaritabelyashova@gmail.com>
2025-12-04 12:29:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,35 @@
-import logging
-import asyncio
-from dotenv import load_dotenv
 import os
-
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
+    MessageHandler,
     ContextTypes,
+    filters
 )
 
-# Загружаем .env
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Логирование
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-
-# Команды -------------------------------------------------------
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Бот работает. Готов к работе с расписанием.")
+    await update.message.reply_text("Бот работает! Привет!")
 
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Команды бота:\n/start — проверить, что бот жив\n/help — помощь")
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
 
-
-# Главная функция запуска ---------------------------------------
 
 async def main():
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     await application.run_polling()
 
 
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
